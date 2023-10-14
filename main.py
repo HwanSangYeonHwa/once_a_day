85d12146532b7ce6b6be18ca76bcf984c2a65774
HwanSangYeonHwa <97436524+HwanSangYeonHwa@users.noreply.github.com>
2023-10-14 12:34:02 +0000

    update

--- main.py
+++ main.py
@@ -12,28 +12,33 @@ request = requests.request(method='get', url='https://www.kr.playblackdesert.com
 request.encoding = None
 html = request.content
 soup = BeautifulSoup(html, 'html.parser')
-data = soup.select('#wrap > div > div.container.guild_profile > article > div.box_list_area > ul')
+data = soup.select('#wrap > div > div.container.guild_profile > article > div.box_list_area > ul')[0]
 
-print(data)
-
-data = soup.select('#wrap > div > div > article > div.tab_container > div > div.event_area > div.event_list')[0]
-table = data.findAll('li')
+table = data.findAll('a')
 
 today = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-events = {'events': [], 'last_update': today.strftime('%Y-%m-%d %H:%M:%S')}
-for event in table:
-    title = event.find_next('strong').find_next('em').getText().split(' (최종 수정')[0].strip()
-    count = event.find_next('span', {'class': 'count'}).getText().replace("  ", " ").strip()
-    if count == "상시":
-        deadline = "-"
-    else:
-        deadline = (today + datetime.timedelta(days=int(count.split(' ')[0])-1)).strftime('%Y-%m-%d')
-    url = event.find_next('a').get('href')
-    thumbnail = event.find_next('img').get('src')
-    meta = get_meta(url)
-    events['events'].append({'title': title, 'deadline': deadline, 'count': count, 'url': url, 'thumbnail': thumbnail, 'meta': meta})
+today.strftime('%Y-%m-%d %H:%M:%S')
+members = {}
+new_list = []
+for item in table:
+    url = item.get('href')
+    name = item.getText()
+    members[name] = url
+    new_list.append(name)
+print(new_list)
 
-with open(os.path.join(BASE_DIR, 'events.json'), 'w+', encoding='utf-8') as json_file:
-    json.dump(events, json_file, ensure_ascii=False, indent='\t')
+register = []
+unregister = []
+
+with open(os.path.join(BASE_DIR, 'member.json'), 'w+', encoding='utf-8') as json_file:
+    old_data = json.load(json_file)
+    for n in new_list:
+        if n in old_data['members']:
+            old_data['members'].remove(n)
+        else:
+            register.append(n)
+    unregister = old_data['members']
+    old_data['members'] = new_list
+    #json.dump(events, json_file, ensure_ascii=False, indent='\t')
 
 print('finish\n')
